d546e798d73416fff1ebe373e8ee92a8b8624b01
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-07-30 03:27:40 +0500

    events

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let clutter = "";
+function makeBubbles() {
+    let clutter = "";
 
 for (let i = 1; i <= 126; i++) {
     // const element = array[i];
@@ -8,3 +9,33 @@ for (let i = 1; i <= 126; i++) {
 
 
 document.querySelector("#pbtm").innerHTML = clutter ;
+}
+makeBubbles()
+
+let timer = 60;
+let score = 0;
+
+function runTimer() {
+  let timerIns  =   setInterval(() => {
+        if (timer >0) {
+            timer-- ;
+            document.querySelector("#timerbox").innerHTML = timer;   
+        }
+        else{
+            clearInterval(timerIns);
+        }
+    }, 1000);
+}
+runTimer()
+
+function getNewHit() {
+    let randomNumber = Math.floor(Math.random() * 10) ;
+     document.querySelector("#hitbutton").innerHTML = randomNumber ;
+}
+getNewHit()
+
+function increaseScore() {
+    score += 10
+    document.querySelector("#scorebox").innerHTML = score ;
+}
+// increaseScore()
