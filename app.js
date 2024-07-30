29ba9b1765dab67f8e0284e2f62325868fa4cfac
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-07-30 21:37:57 +0500

    complete this

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let timer = 60;
+let score = 0;
+let hitRn = 0;
+
 function makeBubbles() {
     let clutter = "";
 
@@ -6,14 +10,9 @@ for (let i = 1; i <= 126; i++) {
     console.log(i);
     clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`
 }
-
-
 document.querySelector("#pbtm").innerHTML = clutter ;
 }
 makeBubbles()
-
-let timer = 60;
-let score = 0;
 
 function runTimer() {
   let timerIns  =   setInterval(() => {
@@ -23,14 +22,16 @@ function runTimer() {
         }
         else{
             clearInterval(timerIns);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`
+            
         }
     }, 1000);
 }
 runTimer()
 
 function getNewHit() {
-    let randomNumber = Math.floor(Math.random() * 10) ;
-     document.querySelector("#hitbutton").innerHTML = randomNumber ;
+    hitRn = Math.floor(Math.random() * 10) ;
+     document.querySelector("#hitbutton").innerHTML = hitRn;
 }
 getNewHit()
 
@@ -38,4 +39,14 @@ function increaseScore() {
     score += 10
     document.querySelector("#scorebox").innerHTML = score ;
 }
-// increaseScore()
+document.querySelector("#pbtm").addEventListener("click" , (dets)=>{
+    // let bubble = document.querySelector(".bubble");
+    // alert("chal raha h")
+    let clickedNum = (Number(dets.target.textContent));
+
+    if (hitRn == clickedNum) {
+        increaseScore();
+        getNewHit();
+        makeBubbles();
+    }
+})
